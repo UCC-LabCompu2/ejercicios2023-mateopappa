9f1fe59252a8002270f28e20bc74bb730f41b989
Mateo Pappalardo <2328796@ucc.edu.ar>
2024-12-10 15:29:18 -0300

    revision ejercicios

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -155,3 +155,31 @@ const mostrarDiv = (divId) => {
 
     }
 };
+
+function calcularsuma(){
+    var num1,num2;
+    num1=Number(document.getElementsByName("sum_num1")[0].value);
+    num2=Number(document.getElementsByName("sum_num2")[0].value);
+    document.getElementsByName("sum_total")[0].innerHTML=num1+num2;
+}
+
+function calcularresta(){
+    var num1,num2;
+    num1=Number(document.getElementsByName("res_num1")[0].value);
+    num2=Number(document.getElementsByName("res_num2")[0].value);
+    document.getElementsByName("res_total")[0].innerHTML=num1-num2;
+}
+
+function calcularproducto() {
+    var num1, num2;
+    num1 = Number(document.getElementsByName("mul_num1")[0].value);
+    num2 = Number(document.getElementsByName("mul_num2")[0].value);
+    document.getElementsByName("mul_total")[0].innerHTML = num1*num2;
+}
+
+function calculardiv() {
+    var num1, num2;
+    num1 = Number(document.getElementsByName("div_num1")[0].value);
+    num2 = Number(document.getElementsByName("div_num2")[0].value);
+    document.getElementsByName("div_total")[0].innerHTML = num1/num2;
+}
